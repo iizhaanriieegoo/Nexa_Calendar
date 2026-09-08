1828b7862be3b3df38693e237803a88b3c05aaab
iizhaanriieegoo <iizhaanriieegoo@gmail.com>
2026-09-09 01:59:48 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-/* Pega aquí la configuración de tu aplicación web de Firebase. */
 window.NEXA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDM8O8biODuEIhv5SIujh9psA3n4q-w2Os",
+  apiKey: "AIzaSyDM08iB0iDuEIhv5SIujh9psA3n4q-w20s",
   authDomain: "nexa-calendar-1adad.firebaseapp.com",
   projectId: "nexa-calendar-1adad",
   storageBucket: "nexa-calendar-1adad.firebasestorage.app",
   messagingSenderId: "467887821955",
-  appId: "1:467887821955:web:47432341a8c4d3705b77b6"
+  appId: "1:467887821955:web:47432143a8c4d3705b77b6",
+  measurementId: "G-QRJNRVE2R"
 };
