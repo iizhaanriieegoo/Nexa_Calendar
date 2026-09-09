55232f57ca2e264a31e322b216d45ddb88203648
iizhaanriieegoo <iizhaanriieegoo@gmail.com>
2026-09-09 02:29:49 +0200

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
+/* Pega aquí la configuración de tu aplicación web de Firebase. */
 window.NEXA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDM8O8biODuEIhv5SIujh9psA3n4q-w2Os",
-  authDomain: "nexa-calendar-1adad.firebaseapp.com",
-  projectId: "nexa-calendar-1adad",
-  storageBucket: "nexa-calendar-1adad.firebasestorage.app",
-  messagingSenderId: "467887821955",
-  appId: "1:467887821955:web:47432341a8c4d3705b77b6",
-  measurementId: "G-QRJNRRVE2R"
+  apiKey: "REPLACE_ME",
+  authDomain: "REPLACE_ME.firebaseapp.com",
+  projectId: "REPLACE_ME",
+  storageBucket: "REPLACE_ME.appspot.com",
+  messagingSenderId: "REPLACE_ME",
+  appId: "REPLACE_ME"
 };
